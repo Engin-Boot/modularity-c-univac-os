bed7d5aa3c46402a7628a4466d6ed28cee2f7219
univac <68467947+univac-os@users.noreply.github.com>
2020-09-08 17:03:21 +0530

    Update GetNumber.cs

--- TelCo.ColorCoder/GetNumber.cs
+++ TelCo.ColorCoder/GetNumber.cs
@@ -10,7 +10,7 @@ namespace TelCo.ColorCoder
 {
   public class GetNumber
   {
-      internal static TelColorCode.ColorPair GetColorFromPairNumber(int pairNumber)
+      internal static TelColorPair.ColorPair GetColorFromPairNumber(int pairNumber)
         {
             // The function supports only 1 based index. Pair numbers valid are from 1 to 25
             int minorSize = colorMapMinor.Length;
