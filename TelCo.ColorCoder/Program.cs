e0a083970da1b705cf4d477992b41ed1f5876fe3
univac <68467947+univac-os@users.noreply.github.com>
2020-09-08 17:33:34 +0530

    Update Program.cs

--- TelCo.ColorCoder/Program.cs
+++ TelCo.ColorCoder/Program.cs
@@ -8,8 +8,8 @@ namespace TelCo.ColorCoder
     {  
         public static void Main(string[] args)
         {
-            int minorSize = ColorCode.colorMapMajor.Length;
-            int majorSize = ColorCode.colorMapMajor.Length;
+            int minorSize = TelColorPair.colorMapMajor.Length;
+            int majorSize = TelColorPair.colorMapMajor.Length;
             int pairNumber = 4;
             TelColorPair.ColorPair testPair1 = GetNumber.GetColorFromPairNumber(pairNumber);
             Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, testPair1);
@@ -28,12 +28,12 @@ namespace TelCo.ColorCoder
             Debug.Assert(testPair1.majorColor == Color.Violet);
             Debug.Assert(testPair1.minorColor == Color.Green);
 
-            TelColorCode.ColorPair testPair2 = new ColorPair() { majorColor = Color.Yellow, minorColor = Color.Green };
+            TelColorCode.ColorPair testPair2 = new TelColorPairColorPair() { majorColor = Color.Yellow, minorColor = Color.Green };
             pairNumber = GetPair.GetPairNumberFromColor(testPair2);
             Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}\n", testPair2, pairNumber);
             Debug.Assert(pairNumber == 18);
 
-            testPair2 = new TelColorCode.ColorPair() { majorColor = Color.Red, minorColor = Color.Blue };
+            testPair2 = new TelColorPair.ColorPair() { majorColor = Color.Red, minorColor = Color.Blue };
             pairNumber = GetPair.GetPairNumberFromColor(testPair2);
             Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}", testPair2, pairNumber);
             Debug.Assert(pairNumber == 6);
