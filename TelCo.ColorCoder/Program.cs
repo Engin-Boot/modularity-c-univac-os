4fb37b61bb0c041ed7673cd5fcb5e7dbc8f5317b
univac <68467947+univac-os@users.noreply.github.com>
2020-09-08 17:42:27 +0530

    Update Program.cs
    keyboard not working good

--- TelCo.ColorCoder/Program.cs
+++ TelCo.ColorCoder/Program.cs
@@ -17,7 +17,7 @@ namespace TelCo.ColorCoder
             Debug.Assert(testPair1.minorColor == Color.Brown);
 
             pairNumber = 5;
-            testPair1 = GetNumber.FromPairNumber(pairNumber);
+            testPair1 = GetNumber.GetColorFromPairNumber(pairNumber);
             Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, testPair1);
             Debug.Assert(testPair1.majorColor == Color.White);
             Debug.Assert(testPair1.minorColor == Color.SlateGray);
@@ -28,7 +28,7 @@ namespace TelCo.ColorCoder
             Debug.Assert(testPair1.majorColor == Color.Violet);
             Debug.Assert(testPair1.minorColor == Color.Green);
 
-            TelColorPair.ColorPair testPair2 = new TelColorPairColorPair() { majorColor = Color.Yellow, minorColor = Color.Green };
+            TelColorPair.ColorPair testPair2 = new TelColorPair.ColorPair() { majorColor = Color.Yellow, minorColor = Color.Green };
             pairNumber = GetPair.GetPairNumberFromColor(testPair2);
             Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}\n", testPair2, pairNumber);
             Debug.Assert(pairNumber == 18);
