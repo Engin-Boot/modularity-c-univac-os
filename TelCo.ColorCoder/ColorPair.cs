ab2dc8d9c719709dde8f264c2ea4077826326358
univac <68467947+univac-os@users.noreply.github.com>
2020-09-08 15:50:38 +0530

    Create ColorPair.cs

---
+++ TelCo.ColorCoder/ColorPair.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace Telco.ColorCoder
+{
+internal class ColorPair:Color
+        {
+            internal Color majorColor;
+            internal Color minorColor;
+            public override string ToString()
+            {
+                return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
+            }
+        }
+        /// <summary>
+        /// Static constructor required to initialize static variable
+        /// </summary>
+        static ColorPair()
+        {
+            colorMapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
+            colorMapMinor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
+        }
+
+        /// <summary>
+        /// Given a pair number function returns the major and minor colors in that order
+        /// </summary>
+        /// <param name="pairNumber">Pair number of the color to be fetched</param>
+        /// <returns></returns>
+}
