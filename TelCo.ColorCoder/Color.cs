251fb63bd6339d8e3d5f7930eda96bd14a34f7d7
univac <68467947+univac-os@users.noreply.github.com>
2020-09-08 15:49:06 +0530

    Update and rename ColorPair.cs to Color.cs

---
+++ TelCo.ColorCoder/Color.cs
@@ -0,0 +1,31 @@
+
+using System;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace TelCo.ColorCoder
+{
+                                /// <summary>
+                                /// The 25-pair color code, originally known as even-count color code, 
+                                  /// is a color code used to identify individual conductors in twisted-pair 
+                                  /// wiring for telecommunications.
+                                  /// This class provides the color coding and 
+                                      /// mapping of pair number to color and color to pair number.
+                                        /// </summary>
+    class Color
+    {
+        /// <summary>
+        /// Array of Major colors
+        /// </summary>
+        #region COLOR
+        private static Color[] colorMapMajor;
+        /// <summary>
+        /// Array of minor colors
+        /// </summary>
+        private static Color[] colorMapMinor;
+        /// <summary>
+        /// data type defined to hold the two colors of clor pair
+        /// </summary>
+        #endregion
+        
+     }
