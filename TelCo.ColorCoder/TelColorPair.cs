1dfcd7ab02f27c2caebecc3eb58afd05e964e1ca
univac <68467947+univac-os@users.noreply.github.com>
2020-09-08 16:52:54 +0530

    Update TelColorPair.cs
    git it

--- TelCo.ColorCoder/TelColorPair.cs
+++ TelCo.ColorCoder/TelColorPair.cs
@@ -35,7 +35,7 @@ namespace TelCo.ColorCoder
         /// <summary>
         /// Static constructor required to initialize static variable
         /// </summary>
-       static ColorPair()
+       static TelColorPair()
         {
             colorMapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
             colorMapMinor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
